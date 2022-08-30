3f6f5ae8f9210afdf0d1235ad2e6c2796512cab1
Shoaib Farooq <89779854+ShoaibFarooq3@users.noreply.github.com>
2022-08-30 14:53:46 +0500

    Delete src directory
    Test

--- src/Auth/LoginHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../Assets/Auth_Styles/login.css'
-export const LoginHeader = () => {
-  return (
-    <div className='LoginHeader d-flex justify-content-start'>
-    <h3>CV Farms</h3>
-    </div>
-  )
-}
--- src/bkup/Endpoints.js
+++
@@ -1,4 +0,0 @@
-const baseUrl=""
-const GettingHomeGallery="https://cdn.contentful.com/spaces/tmrgfa4bb3pg/environments/master/entries?access_token=ryVEZqL5crP__8sb2R4oyzk_sp8L-LX8v-b7XXYvKnY&content_type=home"
-export const Home_Endpoint="https://cdn.contentful.com/spaces/tmrgfa4bb3pg/environments/master/entries?access_token=ryVEZqL5crP__8sb2R4oyzk_sp8L-LX8v-b7XXYvKnY&content_type=home"
-export const About_Endpoint="https://cdn.contentful.com/spaces/tmrgfa4bb3pg/environments/master/entries?access_token=ryVEZqL5crP__8sb2R4oyzk_sp8L-LX8v-b7XXYvKnY&content_type=aboutSection"
